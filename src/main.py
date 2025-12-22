d89de9dbed6152e37be72fa87d1000c66588cfe7
muzaffar-sirpi <129746616+muzaffar-sirpi@users.noreply.github.com>
2025-12-22 10:41:20 +0530

    Allowed Methods Updated

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=[
         "Accept",
         "Accept-Encoding",
